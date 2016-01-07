c4d12708e31103fce1c1dcf1189060a4c4458dd8
Shanyl Jimenez <falsenoglory@gmail.com>
2016-01-07 17:43:47 +0800

    first  commit, done all the layout part but not yet the functionality :(

--- app/src/main/java/sidespell/tech/solarsystem/activity/MainActivity.java
+++ app/src/main/java/sidespell/tech/solarsystem/activity/MainActivity.java
@@ -3,15 +3,19 @@ package sidespell.tech.solarsystem.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import sidespell.tech.solarsystem.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Spinner spnPlanet;
+    private ImageView imgPlanet;
+    private static int[] Planet={R.drawable.mercury,R.drawable.venus,R.drawable.earth, R.drawable.mars,R.drawable.jupiter,R.drawable.saturn,R.drawable.uranus,R.drawable.neptune,R.drawable.pluto};
 
 
     @Override
@@ -21,5 +25,28 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Spinner spnPlanet=(Spinner)findViewById(R.id.spnPlanet);
+        ImageView imgImageView=(ImageView)findViewById(R.id.imgPlanet);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnPlanet.setAdapter(adapter);
     }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+
+        spnPlanet.getAdapter().getCount();
+
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
 }
